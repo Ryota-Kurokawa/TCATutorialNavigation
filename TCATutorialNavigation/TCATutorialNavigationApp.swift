56c97f72bc50400625240d64482fed284e7ab142
Ryota-Kurokawa <ryota-kurokawa@cybozu.co.jp>
2025-04-14 11:21:10 +0900

    Initial Commit

---
+++ TCATutorialNavigation/TCATutorialNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialNavigationApp.swift
+//  TCATutorialNavigation
+//
+//  Created by ryota1582 on 2025/04/14.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCATutorialNavigationTests/TCATutorialNavigationTests.swift
@@ -0,0 +1,16 @@
+//
+//  TCATutorialNavigationTests.swift
+//  TCATutorialNavigationTests
+//
+//  Created by ryota1582 on 2025/04/14.
+//
+
+import Testing
+
+struct TCATutorialNavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
